82e7b7dc36bf62643f055e37e611a34f3b18b35b
Vrajpatel209 <patel.vraju209@gmail.com>
2024-02-02 09:13:35 +0530

    02-02-2024

--- FinalGloMain/Script.js
+++ FinalGloMain/Script.js
@@ -1,10 +1,11 @@
 let SearchValue = [];
+let ind = -1;
 
 function display() {
-    let d = SearchValue.map(function (value) {
+    let d = SearchValue.map(function (value, index) {
         return `<div class="grocery-list">${value}
-        <button class="edit-btn">Edit</button>
-        <button class="delete-btn">Delete</button>
+        <button class="edit-btn" onclick="editItem(${index})">Edit</button>
+        <button class="delete-btn" onclick="removeItem(${index})">Delete</button>
     </div>`;
     });
     document.getElementById("items").innerHTML = d.join(" ");
@@ -12,6 +13,30 @@ function display() {
 
 function addValues() {
     const newValues = document.getElementById("grocery").value;
-    SearchValue.push(newValues);
+    //SearchValue.push(newValues);
+    if (ind < 0) {
+        SearchValue.push(newValues);
+    }
+    else {
+        SearchValue[ind] = newValues;
+    }
+    ind = -1;
     display();
 }
+
+function removeItem(index) {
+    SearchValue.splice(index, 1);
+    display();
+}
+
+function clearAllItems() {
+    SearchValue.splice(0);
+    display();
+}
+
+function editItem(index) {
+    document.getElementById("grocery").value = SearchValue[index];
+    ind = index;
+}
+//HomeWork:
+// create 3text-box(FirstName,LastName,PhoneNo.) with single array i.e. ArrayOfObject.
