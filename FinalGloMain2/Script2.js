79095b546e3d352f4aaeeb8515e8e53cfa86ed68
Vrajpatel209 <patel.vraju209@gmail.com>
2024-02-05 08:53:26 +0530

    5-2-24

--- FinalGloMain2/Script2.js
+++ FinalGloMain2/Script2.js
@@ -1,10 +1,20 @@
-let SearchValue = [];
-let myObject = { FName: '', LName: '', Num: '' }
-let ind = -1;
+let Details = [];
 
+function addValues() {
+    const info = {
+        FirstName : document.getElementById("FName").value,
+        LastName : document.getElementById("LName").value,
+        ContactNo : document.getElementById("Number").value,
+    }
+
+    Details.push(info);
+
+    display();
+}
 function display() {
-    let d = SearchValue.map(function (value, index) {
-        return `<div class="grocery-list">${value}
+    let d = Details.map(function (value, index) {
+        return `<div class="grocery-list">
+        ${value.FirstName} ${value.LastName} : ${value.ContactNo}
         <button class="edit-btn" onclick="editItem(${index})">Edit</button>
         <button class="delete-btn" onclick="removeItem(${index})">Delete</button>
     </div>`;
@@ -12,38 +22,14 @@ function display() {
     document.getElementById("items").innerHTML = d.join(" ");
 }
 
-function addValues() {
-    const newValues1 = document.getElementById("grocery1").value;
-    const newValues2 = document.getElementById("grocery2").value;
-    const newValues3 = document.getElementById("grocery3").value;
-    //SearchValue.push(newValues);
-    if (ind < 0) {
-        SearchValue.push(myObject[newValues1]);
-        SearchValue.push(myObject[newValues2]);
-        SearchValue.push(myObject[newValues3]);
-    }
-    else {
-        SearchValue[ind] = SearchValue.myObject;
-    }
-    ind = -1;
-    display();
-}
-
 function removeItem(index) {
-    SearchValue.splice(index, 1);
+    Details.splice(index, 1);
     display();
 }
 
 function clearAllItems() {
-    SearchValue.splice(0);
+    Details.splice(0);
     display();
-}
-
-function editItem(index) {
-    document.getElementById("grocery1").value = SearchValue.myObject[index];
-    document.getElementById("grocery2").value = SearchValue.myObject[index];
-    document.getElementById("grocery3").value = SearchValue.myObject[index];
-    ind = index;
 }
 //HomeWork:
 // create 3text-box(FirstName,LastName,PhoneNo.) with single array i.e. ArrayOfObject.
